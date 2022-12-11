fixed the computation of MAP

scripts/recon/recon_map.py
@@ -35,9 +35,9 @@ def main(config_path):
 
         if mask is None:
             if not config['poisson']:
-                mask = sampling_pattern.gen_mask_2D(nx, ny, center_r = config['cal'], undersampling = config['sampling_rate'])
+                mask = sampling_pattern.gen_mask_2d_equally_skip(factor_x=config['fx'], factor_y=config['fy'])
             else:
-                mask = utils.bart(1, 'poisson -Y %d -Z %d -y %f -z %f -s 1234 -v -C %d'%(nx, ny, config['fx'], config['fy'], config['cal']))
+                mask = utils.bart(1, 'poisson -Y %d -Z %d -y %f -z %f -s 1234 -C %d'%(nx, ny, config['fx'], config['fy'], config['cal']))
                 mask = np.squeeze(mask)
 
         und_ksp = kspace*abs(mask[..., np.newaxis])

spreco/common/utils.py
@@ -9,6 +9,7 @@ import yaml
 
 import subprocess as sp
 import numpy as np
+import cupy as cp
 from skimage.metrics import structural_similarity, peak_signal_noise_ratio
 import matplotlib
 matplotlib.use('Agg')
@@ -16,10 +17,16 @@ import matplotlib.pyplot as plt
 
 
 def float2cplx(float_in):
-    return np.array(float_in[...,0]+1.0j*float_in[...,1], dtype='complex64')
+    if isinstance(float_in, np.ndarray):
+        return np.array(float_in[...,0]+1.0j*float_in[...,1], dtype='complex64')
+    else:
+        return cp.asarray(float_in[...,0]+1.0j*float_in[...,1], dtype='complex64')
 
 def cplx2float(cplx_in):
-    return np.array(np.stack((cplx_in.real, cplx_in.imag), axis=-1), dtype='float32')
+    if isinstance(cplx_in, np.ndarray):
+        return np.array(np.stack((cplx_in.real, cplx_in.imag), axis=-1), dtype='float32')
+    else:
+        return cp.asarray(cp.stack((cplx_in.real, cplx_in.imag), axis=-1), dtype='float32')
 
 def log_to(file, vars, mode="a", clear=False, end='\n', prefix=None):
     if clear:
@@ -84,6 +91,11 @@ def check_out(cmd, split=True):
 def find_files(path, pattern):
     cmd = "find " + path + " -type f -name " + pattern
     return check_out(cmd)
+
+def read_filelist(filename):
+    with open(filename) as f:
+        lines = [line.rstrip() for line in f]
+        return lines
 
 def list_data_sys(path, suffix, ex_pattern=None):
     """
@@ -180,7 +192,10 @@ def normalize_with_max(x, axis=(0,1), data_chns='CPLX'):
     x is complex value
     x = x/(max(abs(x)))
     """
-    scalor = np.max(abs(x), axis)
+    if isinstance(x, np.ndarray):
+        scalor = np.max(abs(x), axis)
+    else:
+        scalor = cp.max(abs(x), axis)
 
     if data_chns == 'CPLX':
         normalized_x = cplx2float(x/scalor)
@@ -398,7 +413,7 @@ def norm_to_uint(inp, bit=8):
 
 def psnr(img1, img2, bit=16, tobit=False):
     """
-    calculate peak SNR 
+    calculate peak SNR, img1-true, img2-test
     """
     pixel_max = np.max(img2)
     

spreco/model/sde.py
@@ -7,6 +7,7 @@ from spreco.common import utils, ops
 import tqdm
 import tensorflow.compat.v1 as tf
 import numpy as np
+import cupy as cp
 
 class sde():
     """
@@ -179,17 +180,16 @@ class sde():
 
 class posterior_sampler():
 
-    def __init__(self, sde, steps, target_snr, nr_samples, burn_in, burn_t, map_end=False, last_iteration=100, last_step_factor=1, disable_z=False, use_pixelcnn=False, pixelcnn_reg=None):
+    def __init__(self, sde, steps, target_snr, nr_samples, burn_in, burn_t, ode=False, ext_iter=0, disable_z=False, use_pixelcnn=False, pixelcnn_reg=None):
         self.sde        = sde
+        self.ode        = ode
         self.steps      = steps
         self.target_snr = target_snr
         self.nr_samples = nr_samples
         self.burn_in    = burn_in
         self.burn_t     = burn_t
         self.burn_flag  = True
-        self.map_end    = map_end
-        self.last_iteration   = last_iteration
-        self.last_step_factor = last_step_factor
+        self.ext_iter   = ext_iter
         self.disable_z        = disable_z
         self.use_pixelcnn     = use_pixelcnn
         self.pixelcnn_reg     = pixelcnn_reg
@@ -300,8 +300,13 @@ class posterior_sampler():
 
                 grad_data_fidelity = AHA(utils.float2cplx(x_val))
                 grad_data_fidelity = utils.cplx2float(grad_data_fidelity)
-
-                x_val = x_val + tau*score - std*s_stepsize*grad_data_fidelity + s_stepsize*std*noisy_x + noise
+                if isinstance(grad_data_fidelity, cp.ndarray):
+                    grad_data_fidelity = cp.asnumpy(grad_data_fidelity)
+                    noisy_x = cp.asnumpy(noisy_x)
+                if self.ode:
+                    x_val = x_val + tau*score - std*s_stepsize*grad_data_fidelity + s_stepsize*std*noisy_x
+                else:
+                    x_val = x_val + tau*score - std*s_stepsize*grad_data_fidelity + s_stepsize*std*noisy_x + noise
 
                 if self.use_pixelcnn:
                     scale = np.max(abs(utils.float2cplx(x_val)))
@@ -316,26 +321,18 @@ class posterior_sampler():
 
                 xs.append(x_val)
 
-        if self.map_end:
-            t_i = t_vals[-1]
-            for _ in range(self.last_iteration):
+        if self.ext_iter != 0:
+            t_i = t_vals[-2]
+            for _ in range(self.ext_iter):
 
-                if self.burn_in and t_i > self.burn_t:
-                    tau, _, noise = sess.run(get_noise_op_1, feed_dict={x: x_val, t: [t_i]*cur_samples})
-                else:
-                    tau, _, noise = sess.run(get_noise_op_2, feed_dict={x: x_val, t: [t_i]*cur_samples})
-
+                tau, _, noise = sess.run(get_noise_op_2, feed_dict={x: x_val, t: [t_i]*cur_samples})
                 score =  self.target_snr*sess.run(score_op,  {x: x_val, t: [t_i]*cur_samples})
-                
-                float_shape = x_val.shape
-                _, sigma = self.sde.marginal_prob(AHy, t_i)
-                z = utils.float2cplx(sess.run(tf.random.normal(float_shape, seed=self.sde.seed)*sigma))
-                noisy_x = utils.cplx2float(AHy + np.squeeze(AHA(z)))
+                x_ = utils.cplx2float(AHy)
 
-                grad_data_fidelity = AHA(utils.float2cplx(x_val+tau*score))
+                grad_data_fidelity = AHA(utils.float2cplx(x_val))
                 grad_data_fidelity = utils.cplx2float(grad_data_fidelity)
 
-                x_val = x_val + tau*score - std*s_stepsize*grad_data_fidelity*self.last_step_factor + s_stepsize*std*noisy_x*self.last_step_factor + noise
+                x_val = x_val + tau*score - std*s_stepsize*grad_data_fidelity + s_stepsize*std*x_
 
                 xs.append(x_val)
 