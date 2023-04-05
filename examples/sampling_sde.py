c6c82d22fd5ec70b490bec2a488f3cd769b15a2c
luo9 <guanxiong.luo@med.uni-goettingen.de>
2023-04-05 15:15:43 +0200

    updated sampling_sde.py

--- examples/sampling_sde.py
+++ examples/sampling_sde.py
@@ -5,24 +5,24 @@ import numpy as np
 import os
 import sys
 
-eval_path = lambda pr, ch: os.path.join(pr, ch)
+evalpath = lambda pr, ch: os.path.join(pr, ch)
+savecfl  = lambda p, a: utils.writecfl(p, utils.float2cplx(a))
 
 s_config = utils.load_config(sys.argv[1])
 
-model_path = eval_path(s_config['log_folder'], s_config['model_name'])
+model_path = evalpath(s_config['log_folder'], s_config['model_name'])
 config     = utils.load_config(s_config['log_folder']+'/config.yaml')
-save_path  = utils.create_folder(eval_path(s_config['log_folder'], 'samples'))
+save_path  = utils.create_folder(evalpath(s_config['log_folder'], 'samples'))
 utils.save_config(s_config, save_path)
 
 print("INFO -> sigma type: %s, sigma max: %.4f, simga min: %.4f, discrete steps: %d "%(s_config['sigma_type'], config['sigma_max'], config['sigma_min'], s_config['N']))
-a_sampler = sampler(config, s_config['target_snr'], s_config['sigma_type'], s_config['N'])
-
+a_sampler = sampler(config, s_config['target_snr'], s_config['sigma_type'], s_config['N'], s_config['T'])
 a_sampler.init_sampler(model_path, gpu_id=s_config['gpu_id'])
 
 image_n, image  = a_sampler.pc_sampler(s_config['nr_samples'], s_config['steps_pc'])
-utils.writecfl(eval_path(save_path, 'pc'), utils.float2cplx(np.array(image[-1])))
-utils.writecfl(eval_path(save_path, 'pc'), utils.float2cplx(np.array(image_n[-1])))
+savecfl(evalpath(save_path, 'pc'), image[-1])
+savecfl(evalpath(save_path, 'pc_n'), image_n[-1])
 
 image_n, image  = a_sampler.ancestral_sampler(s_config['nr_samples'], s_config['steps_an'])
-utils.writecfl(eval_path(save_path, 'an'), utils.float2cplx(np.array(image[-1])))
-utils.writecfl(eval_path(save_path, 'an'), utils.float2cplx(np.array(image_n[-1])))
+savecfl(evalpath(save_path, 'an'), image[-1])
+savecfl(evalpath(save_path, 'an_n'), image_n[-1])
