06f345cbe586d0ba1f6e374dfa7d72fa4762ee84
luo9 <guanxiong.luo@med.uni-goettingen.de>
2023-04-13 10:27:04 +0200

    updated exporter.py

--- spreco/exporter.py
+++ spreco/exporter.py
@@ -8,14 +8,14 @@ tf.disable_eager_execution()
 
 class exporter():
     """
+    TODO: freeze graph before exporting
     """
 
-    def __init__(self, log, meta, path, name, sigma_type=None, default_out=True, **kwargs):
+    def __init__(self, log, meta, path, name, default_out=True, **kwargs):
         self.log    = log
         self.meta   = meta
         self.path   = path
         self.name   = name
-        self.sigma_type = sigma_type # for sde model
         self.init(default_out, kwargs)
 
     def init(self, default, kwargs):
@@ -27,34 +27,40 @@ class exporter():
         
         if config['model'] == MODELS.NCSN:
             from spreco.model.ncsn import ncsn as selected_class
+            sigma_type = None
 
         elif config['model'] == MODELS.SDE:
             from spreco.model.sde import sde as selected_class
+            if 'sigma_max' in kwargs.keys() and kwargs['sigma_max'] <= config['sigma_max']:
+                config['sigma_max'] = kwargs['sigma_max']
+            else:
+                raise ValueError('Sigma max is not acceptiable for score net!')
+        
+            if 'sigma_min' in kwargs.keys() and kwargs['sigma_min'] >= config['sigma_min']:
+                config['sigma_min'] = kwargs['sigma_min']
+            else:
+                raise ValueError('Sigma min is not acceptiable for score net!')
+            
+            if 'sigma_type' in kwargs.keys():
+                sigma_type = kwargs['sigma_type']
 
         elif config['model'] == MODELS.PIXELCNN:
             from spreco.model.pixelcnn import pixelcnn as selected_class
+            sigma_type = None
 
         else:
             raise Exception("Currently, this model is not implemented!")
-        
-        if 'sigma_max' in kwargs.keys() and kwargs['sigma_max'] <= config['sigma_max']:
-            config['sigma_max'] = kwargs['sigma_max']
-        else:
-            raise ValueError('Sigma max is not acceptiable for score net!')
-    
-        if 'sigma_min' in kwargs.keys() and kwargs['sigma_min'] >= config['sigma_min']:
-            config['sigma_min'] = kwargs['sigma_min']
-        else:
-            raise ValueError('Sigma min is not acceptiable for score net!')
 
 
         os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-        os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
+        if 'gpu_id' in kwargs.keys():
+            os.environ["CUDA_VISIBLE_DEVICES"] = kwargs['gpu_id']
+        else:
+            os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
         
         self.model = selected_class(config)
-        self.model.config['sigma_type'] = self.sigma_type
 
-        self.model.init(mode=2, default_out=default)
+        self.model.init(mode=2, default_out=default, sigma_type=sigma_type)
 
         if default:
             self.restore()
